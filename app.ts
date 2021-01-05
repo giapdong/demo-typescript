3c9755668bb8db35472d88909759d3de1f382144
Nam Nguyen <giapdong992@gmail.com>
2021-01-05 09:38:10 +0700

    Add alias @ as root directory for compile and import module

--- app.ts
+++ app.ts
@@ -8,8 +8,8 @@ import * as bodyparser from "body-parser";
 import * as winston from "winston";
 import * as expressWinston from "express-winston";
 import cors from "cors";
-import { CommonRoutesConfig } from "./common/common.routes.config";
-import { UsersRoutes } from "./users/users.routes.config";
+import { CommonRoutesConfig } from "@/common/common.routes.config";
+import { UsersRoutes } from "@/users/users.routes.config";
 import debug from "debug";
 
 const app: Application = express();
