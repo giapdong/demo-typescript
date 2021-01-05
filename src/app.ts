aed3e84bde7bf5085b41b638e27e71109dcaf8e3
Nam Nguyen <giapdong992@gmail.com>
2021-01-05 15:16:47 +0700

    Format same morgan for winston

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,17 @@ import "module-alias/register";
 import "@babel/polyfill";
 import express from "express";
 import { Application } from "express";
+import cors from "cors";
 import * as http from "http";
 import * as bodyparser from "body-parser";
 
-import * as winston from "winston";
+import debug from "debug";
 // import * as morgan from "morgan";
+import * as winston from "winston";
 import * as expressWinston from "express-winston";
-import cors from "cors";
+
 import { CommonRoutesConfig } from "@root/common/common.routes.config";
 import { UsersRoutes } from "@root/users/users.routes.config";
-import debug from "debug";
 
 const app: Application = express();
 const server: http.Server = http.createServer(app);
@@ -29,7 +30,18 @@ app.use(cors());
 app.use(
   expressWinston.logger({
     transports: [new winston.transports.Console()],
-    format: winston.format.combine(winston.format.colorize(), winston.format.json())
+    format: winston.format.combine(
+      winston.format.label({ label: "HTTP" }),
+      winston.format.timestamp(),
+      winston.format.printf(({ level, message, label, timestamp }) => {
+        return `${level}: [${label}] ${timestamp} ${message}`;
+      })
+    ),
+    msg: "{{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) {
+      return false;
+    }
   })
 );
 
@@ -52,6 +64,7 @@ app.get("/", (req: express.Request, res: express.Response) => {
 });
 
 server.listen(port, () => {
+  console.log("Hihi, server started!");
   debugLog(`Server running at http://localhost:${port}`);
   routes.forEach((route: CommonRoutesConfig) => {
     debugLog(`Routes configured for ${route.getName()}`);
