46e5356e06d1052f98ddff66f984ef6164263979
Nam Nguyen <giapdong992@gmail.com>
2021-01-21 11:14:08 +0700

    Demo call api

--- src/utilities/enviroments.ts
+++ src/utilities/enviroments.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import * as path from "path";
 dotenv.config({ path: "enviroments/.env" });
 
-export const APP_ENV = process.env.APP_ENV; // development|production
+export const APP_ENV = process.env.APP_ENV; // development | production
 export const IS_PRODUCTION = APP_ENV === "production";
 export const APP_PORT = process.env.APP_PORT || 8088;
 export const LOG_DIRECTORY = process.env.LOG_DIRECTORY || path.resolve("logs");
